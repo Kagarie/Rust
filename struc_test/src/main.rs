bec576296186989976224ab4e5b8cb18eff30607
Kagari <aymeric.adenot27@orange.fr>
2020-03-05 16:34:47 +0100

    Update

--- struc_test/src/main.rs
+++ struc_test/src/main.rs
@@ -1,11 +1,12 @@
-extern  Personnage;
+extern crate Personnage;
 
 fn main() {
-    let perso = Personnage{
-    nom:"test",
-    pts:20,
-    attaque:5,
-    def:5,
-    };
+    let perso= Personnage.persoCrate();
+    //{
+    //nom:"test",
+    //pts:20,
+    //attaque:5,
+    //def:5,
+    //};
     println!("{:?}", perso);
 }
---
+++ struc_test/target/debug/deps/struc_test-0f058e0b4698e8e2.d
@@ -0,0 +1,5 @@
+/stockage/kraken/Rust/struc_test/target/debug/deps/struc_test-0f058e0b4698e8e2: src/main.rs
+
+/stockage/kraken/Rust/struc_test/target/debug/deps/struc_test-0f058e0b4698e8e2.d: src/main.rs
+
+src/main.rs:
